43e1cd46b2b7abae6339e7baf0dda64c601874d9
lcruan <1416802331@qq.com>
2026-07-12 17:17:17 +0800

    'LCD1602显示一个字符'

---
+++ 14.LCD1602显示一个字符/Drivers/BSP/lcd1602/lcd1602.c
@@ -0,0 +1,105 @@
+#include "lcd1602.h"
+#include "delay.h"
+
+// RS引脚定义
+#define RS_GPIO_Port    GPIOB
+#define RS_GPIO_Pin     GPIO_PIN_1
+#define RS_HIGH     HAL_GPIO_WritePin(RS_GPIO_Port, RS_GPIO_Pin, GPIO_PIN_SET);
+#define RS_LOW      HAL_GPIO_WritePin(RS_GPIO_Port, RS_GPIO_Pin, GPIO_PIN_RESET);
+
+// RW引脚定义
+#define RW_GPIO_Port    GPIOB
+#define RW_GPIO_Pin     GPIO_PIN_2
+#define RW_HIGH     HAL_GPIO_WritePin(RW_GPIO_Port, RW_GPIO_Pin, GPIO_PIN_SET);
+#define RW_LOW      HAL_GPIO_WritePin(RW_GPIO_Port, RW_GPIO_Pin, GPIO_PIN_RESET);
+// EN引脚定义
+#define EN_GPIO_Port    GPIOB
+#define EN_GPIO_Pin     GPIO_PIN_10
+#define EN_HIGH     HAL_GPIO_WritePin(EN_GPIO_Port, EN_GPIO_Pin, GPIO_PIN_SET);
+#define EN_LOW      HAL_GPIO_WritePin(EN_GPIO_Port, EN_GPIO_Pin, GPIO_PIN_RESET);
+
+void lcd1602_init(void)
+{
+    // 初始化GPIO口
+    lcd1602_gpio_init();
+    // 上电初始化
+    lcd1602_start();
+}
+
+void lcd1602_gpio_init(void)
+{
+    GPIO_InitTypeDef gpio_initstruct;
+    __HAL_RCC_GPIOB_CLK_ENABLE();
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+    
+    gpio_initstruct.Pin = GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2
+                        | GPIO_PIN_3 | GPIO_PIN_4 | GPIO_PIN_5
+                        | GPIO_PIN_6 | GPIO_PIN_7;
+    gpio_initstruct.Mode = GPIO_MODE_OUTPUT_PP; // 高低电平都需要，使用推挽输出
+    gpio_initstruct.Pull = GPIO_PULLUP; // 上拉
+    gpio_initstruct.Speed = GPIO_SPEED_FREQ_HIGH;
+    HAL_GPIO_Init(GPIOA, &gpio_initstruct);
+    
+    gpio_initstruct.Pin = GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_10;
+    gpio_initstruct.Mode = GPIO_MODE_OUTPUT_PP; // 高低电平都需要，使用推挽输出
+    gpio_initstruct.Pull = GPIO_PULLUP; // 上拉
+    gpio_initstruct.Speed = GPIO_SPEED_FREQ_HIGH;
+    HAL_GPIO_Init(GPIOB, &gpio_initstruct);
+    
+    
+}
+
+void lcd1602_start(void)
+{
+    // (1) 延时15ms
+    delay_ms(15);
+    // (2) 写指令 38H(不检测忙信号)
+    lcd1602_write_cmd(0x38);
+    // (3) 延时5ms
+    delay_ms(5);
+    // (4) 以后每次写指令，读/写数据操作均需要检测忙信号
+    // (5) 写指令 38H：显示模式设置
+    lcd1602_write_cmd(0x38);
+    // (6) 写指令 08H: 显示关闭
+    lcd1602_write_cmd(0x08);
+    // (7) 写指令 01H：显示清屏
+    lcd1602_write_cmd(0x01);
+    // (8) 写指令 06H：显示光标移动设置
+    lcd1602_write_cmd(0x06);
+    // (9) 写指令 0CH: 显示开及光标设置
+    lcd1602_write_cmd(0x0C);
+}
+
+// 写指令
+void lcd1602_write_cmd(char cmd)
+{
+    RS_LOW;
+    RW_LOW;
+    EN_LOW;
+    GPIOA->ODR = cmd;
+    delay_ms(5);
+    EN_HIGH;
+    delay_ms(5);
+    EN_LOW;
+}
+
+// 写数据
+void lcd1602_write_data(char dataShow)
+{
+    RS_HIGH;
+    RW_LOW;
+    EN_LOW;
+    GPIOA->ODR = dataShow;
+    delay_ms(5);
+    EN_HIGH;
+    delay_ms(5);
+    EN_LOW;
+}
+
+void lcd1602_show_char()
+{
+    // 在哪里显示
+    lcd1602_write_cmd(0x80+0x02);
+    // 显示什么
+    lcd1602_write_data('L');
+}
---
+++ 14.LCD1602显示一个字符/Users/main.c
@@ -0,0 +1,25 @@
+#include "sys.h"
+#include "delay.h"
+#include "led.h"
+#include "lcd1602.h"
+
+void led_init(void);                       /* LED初始化函数声明 */
+
+int main(void)
+{
+    HAL_Init();                         /* 初始化HAL库 */
+    stm32_clock_init(RCC_PLL_MUL9); /* 设置时钟, 72Mhz */
+    led_init();
+    lcd1602_init();
+    lcd1602_show_char();
+    while(1)
+    { 
+//        led1_on();
+//        led2_off();
+//        delay_ms(500);
+//        led1_off();
+//        led2_on();
+//        delay_ms(500);
+    }
+}
+
