5a589430ebe098a58cd2dc206713bf477667dbff
lcruan <1416802331@qq.com>
2026-08-01 16:01:54 +0800

    'ESP8266入网功能实现'

---
+++ 37.ESP8266入网功能实现/Drivers/BSP/esp8266/esp8266.h
@@ -0,0 +1,33 @@
+#ifndef __ESP8266_H__
+#define __ESP8266_H__
+
+#include "sys.h"
+
+#define ESP8266_RX_BUF_SIZE         128
+#define ESP8266_TX_BUF_SIZE         64
+
+//´íÎóÂë
+#define ESP8266_EOK                 0
+#define ESP8266_ERROR               1
+#define ESP8266_ETIMEOUT            2
+#define ESP8266_EINVAL              3
+
+#define ESP8266_STA_MODE            1
+#define ESP8266_AP_MODE             2
+#define ESP8266_STA_AP_MODE         3
+
+#define ESP8266_SINGLE_CONNECTION   0
+#define ESP8266_MULTI_CONNECTION    1
+
+#define WIFI_SSID                   ""
+#define WIFI_PWD                    ""
+
+void esp8266_init(uint32_t baudRate);
+void esp8266_receive_data(void);
+void esp8266_test(void);
+
+
+#endif
+
+
+
