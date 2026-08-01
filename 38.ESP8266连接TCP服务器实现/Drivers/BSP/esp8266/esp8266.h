33188af4906f945509c571b65a16f6d3c251d397
lcruan <1416802331@qq.com>
2026-08-01 16:59:15 +0800

    'esp8266连接TCP服务器实现'

--- 38.ESP8266连接TCP服务器实现/Drivers/BSP/esp8266/esp8266.h
+++ 38.ESP8266连接TCP服务器实现/Drivers/BSP/esp8266/esp8266.h
@@ -19,8 +19,11 @@
 #define ESP8266_SINGLE_CONNECTION   0
 #define ESP8266_MULTI_CONNECTION    1
 
-#define WIFI_SSID                   "ChinaNet-LCRK4V"
-#define WIFI_PWD                    "rrz20190923"
+#define WIFI_SSID                   ""
+#define WIFI_PWD                    ""
+
+#define TCP_SERVER_IP               "192.168.101.36"
+#define TCP_SERVER_PORT             "8080"
 
 void esp8266_init(uint32_t baudRate);
 void esp8266_receive_data(void);
