7ce908acc7593bf66e13cdacf2daa9aa73febd2b
lcruan <1416802331@qq.com>
2026-08-09 11:13:49 +0800

    'OLED实验'

---
+++ 49.OLED实验/Drivers/BSP/oled/oled.c
@@ -0,0 +1,157 @@
+#include "oled.h"
+#include "delay.h"
+
+void oled_gpio_init()
+{
+    GPIO_InitTypeDef gpio_initstruct;
+
+    OLED_I2C_SCL_CLK();
+    OLED_I2C_SDA_CLK();
+
+    gpio_initstruct.Pin = OLED_I2C_SCL_PIN;
+    gpio_initstruct.Mode = GPIO_MODE_OUTPUT_PP;
+    gpio_initstruct.Pull = GPIO_PULLUP;
+    gpio_initstruct.Speed = GPIO_SPEED_FREQ_HIGH;
+    HAL_GPIO_Init(OLED_I2C_SCL_PORT, &gpio_initstruct);
+    
+    gpio_initstruct.Pin = OLED_I2C_SDA_PIN;
+    HAL_GPIO_Init(OLED_I2C_SDA_PORT, &gpio_initstruct);
+}
+
+// 起始
+void oled_i2c_start(void)
+{
+    // 两个都是高电平
+    OLED_SCL_SET();
+    OLED_SDA_SET();
+    
+    OLED_SDA_RESET();
+    OLED_SCL_RESET();
+    
+    
+}
+
+// 终止
+void oled_i2c_stop(void)
+{
+    OLED_SCL_SET();
+    OLED_SDA_RESET(); // SDA 由低变高
+    OLED_SDA_SET();
+}   
+
+void oled_i2c_ack(void)
+{
+    OLED_SCL_SET();
+    OLED_SCL_RESET();
+}  
+
+void oled_i2c_write_byte(uint8_t data)
+{
+    uint8_t i, tmp;
+    tmp = data;
+    for(i = 0; i < 8; i++)
+    {
+        // tmp & 0x80取出最高位
+        if ((tmp & 0x80) == 0x80)
+            OLED_SDA_SET();
+        else
+            OLED_SDA_RESET();
+        tmp = tmp << 1;
+        OLED_SCL_SET();
+        OLED_SCL_RESET();
+    }
+}
+
+
+void oled_write_cmd(uint8_t cmd)
+{
+    oled_i2c_start();
+    oled_i2c_write_byte(0x78);
+    oled_i2c_ack();
+    oled_i2c_write_byte(0x00);
+    oled_i2c_ack();
+    oled_i2c_write_byte(cmd);
+    oled_i2c_ack();
+    oled_i2c_stop();
+}
+
+void oled_write_data(uint8_t data)
+{
+    oled_i2c_start();
+    oled_i2c_write_byte(0x78);
+    oled_i2c_ack();
+    oled_i2c_write_byte(0x40);
+    oled_i2c_ack();
+    oled_i2c_write_byte(data);
+    oled_i2c_ack();
+    oled_i2c_stop();
+}
+
+void oled_init(void)
+{
+    oled_gpio_init();
+    
+    delay_ms(100);
+    
+    oled_write_cmd(0xAE);    //设置显示开启/关闭，0xAE关闭，0xAF开启
+
+    oled_write_cmd(0xD5);    //设置显示时钟分频比/振荡器频率
+    oled_write_cmd(0x80);    //0x00~0xFF
+
+    oled_write_cmd(0xA8);    //设置多路复用率
+    oled_write_cmd(0x3F);    //0x0E~0x3F
+
+    oled_write_cmd(0xD3);    //设置显示偏移
+    oled_write_cmd(0x00);    //0x00~0x7F
+
+    oled_write_cmd(0x40);    //设置显示开始行，0x40~0x7F
+
+    oled_write_cmd(0xA1);    //设置左右方向，0xA1正常，0xA0左右反置
+
+    oled_write_cmd(0xC8);    //设置上下方向，0xC8正常，0xC0上下反置
+
+    oled_write_cmd(0xDA);    //设置COM引脚硬件配置
+    oled_write_cmd(0x12);
+
+    oled_write_cmd(0x81);    //设置对比度
+    oled_write_cmd(0xCF);    //0x00~0xFF
+
+    oled_write_cmd(0xD9);    //设置预充电周期
+    oled_write_cmd(0xF1);
+
+    oled_write_cmd(0xDB);    //设置VCOMH取消选择级别
+    oled_write_cmd(0x30);
+
+    oled_write_cmd(0xA4);    //设置整个显示打开/关闭
+
+    oled_write_cmd(0xA6);    //设置正常/反色显示，0xA6正常，0xA7反色
+
+    oled_write_cmd(0x8D);    //设置充电泵
+    oled_write_cmd(0x14);
+
+    oled_write_cmd(0xAF);    //开启显示
+}
+
+void oled_set_cursor(uint8_t x, uint8_t y)
+{
+    oled_write_cmd(0xB0 + y);
+    oled_write_cmd((x & 0x0F) | 0x00);
+    oled_write_cmd(((x & 0xF0) >> 4) | 0x10);
+}
+
+// 清屏函数
+void oled_fill(uint8_t data)
+{
+    uint8_t i, j;
+    // 外层循环8个page
+    for(i = 0; i < 8; i++)
+    {
+        oled_set_cursor(0, i);
+        for (j = 0; j < 128; j++)
+            oled_write_data(data);
+    }
+}
+
+
+
+
---
+++ 49.OLED实验/Users/main.c
@@ -0,0 +1,40 @@
+#include "sys.h"
+#include "delay.h"
+#include "led.h"
+#include "uart1.h"
+#include "oled.h"
+
+void led_init(void);                       /* LED初始化函数声明 */
+
+int main(void)
+{
+    HAL_Init();                         /* 初始化HAL库 */
+    stm32_clock_init(RCC_PLL_MUL9); /* 设置时钟, 72Mhz */
+    led_init();
+    uart1_init(115200);
+    oled_init();
+    printf("hello world!\r\n");
+    
+    oled_fill(0x00);
+    oled_set_cursor(0, 0);
+    
+//    oled_write_cmd(0xB0);
+//    oled_write_cmd(0x00);
+//    oled_write_cmd(0x10);
+    
+    oled_write_data(0x80);
+    oled_write_data(0x80);
+    oled_write_data(0x80);
+    oled_write_data(0x80);
+    oled_write_data(0x80);
+    oled_write_data(0x80);
+    oled_write_data(0x80);
+    oled_write_data(0x80);
+    oled_write_data(0x80);
+    
+    while(1)
+    { 
+        
+    }
+}
+
