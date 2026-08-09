1dd7ddbfe139d835f8f37adc505d89e276dc0269
lcruan <1416802331@qq.com>
2026-08-09 11:34:06 +0800

    'OLED实验-显示字母'

--- 49.OLED实验/Users/main.c
+++ 49.OLED实验/Users/main.c
@@ -16,21 +16,39 @@ int main(void)
     printf("hello world!\r\n");
     
     oled_fill(0x00);
-    oled_set_cursor(0, 0);
     
 //    oled_write_cmd(0xB0);
 //    oled_write_cmd(0x00);
 //    oled_write_cmd(0x10);
     
-    oled_write_data(0x80);
-    oled_write_data(0x80);
-    oled_write_data(0x80);
-    oled_write_data(0x80);
-    oled_write_data(0x80);
-    oled_write_data(0x80);
-    oled_write_data(0x80);
-    oled_write_data(0x80);
-    oled_write_data(0x80);
+//    oled_write_data(0x80);
+//    oled_write_data(0x80);
+//    oled_write_data(0x80);
+//    oled_write_data(0x80);
+//    oled_write_data(0x80);
+//    oled_write_data(0x80);
+//    oled_write_data(0x80);
+//    oled_write_data(0x80);
+//    oled_write_data(0x80);
+    
+    oled_set_cursor(0, 0);
+    oled_write_data(0x00);
+    oled_write_data(0x00);
+    oled_write_data(0xC0);
+    oled_write_data(0x38);
+    oled_write_data(0xE0);
+    oled_write_data(0x00);
+    oled_write_data(0x00);
+    oled_write_data(0x00);
+    oled_set_cursor(0, 1);
+    oled_write_data(0x20);
+    oled_write_data(0x3C);
+    oled_write_data(0x23);
+    oled_write_data(0x02);
+    oled_write_data(0x02);
+    oled_write_data(0x27);
+    oled_write_data(0x38);
+    oled_write_data(0x20);
     
     while(1)
     { 
