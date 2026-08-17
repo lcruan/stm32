f86432cec2d767c0ab0584964880384bd7bf11d7
lcruan <1416802331@qq.com>
2026-08-17 22:13:29 +0800

    '低功耗实验'

---
+++ 57.低功耗实验/Drivers/BSP/lpwr/lpwr.c
@@ -0,0 +1,31 @@
+#include "lpwr.h"
+
+
+// 初始化
+void lpwr_init(void)
+{
+    
+}
+
+// 睡眠模式
+void lpwr_enter_sleep(void)
+{
+    
+}
+
+// 停机模式
+void lpwr_enter_stop(void)
+{
+
+}
+
+// 待机模式
+void lpwr_enter_standby(void)
+{
+    
+}
+    
+
+
+
+
---
+++ 57.低功耗实验/Drivers/BSP/lpwr/lpwr.h
@@ -0,0 +1,10 @@
+#ifndef __LPWR_H__
+#define __LPWR_H__
+
+#include "sys.h"
+
+
+
+
+
+#endif
