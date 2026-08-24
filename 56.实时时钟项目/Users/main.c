2c2d368ddb892cb6d1bc3b163ef41f01e3738c63
lcruan <1416802331@qq.com>
2026-08-24 22:04:45 +0800

    '实时时钟项目'

---
+++ 56.实时时钟项目/Users/main.c
@@ -0,0 +1,26 @@
+#include "sys.h"
+#include "delay.h"
+#include "led.h"
+#include "uart1.h"
+
+void led_init(void);                       /* LED初始化函数声明 */
+
+int main(void)
+{
+    HAL_Init();                         /* 初始化HAL库 */
+    stm32_clock_init(RCC_PLL_MUL9); /* 设置时钟, 72Mhz */
+    led_init();
+    uart1_init(115200);
+    printf("hello world!\r\n");
+    
+    while(1)
+    { 
+        led1_on();
+        led2_off();
+        delay_ms(500);
+        led1_off();
+        led2_on();
+        delay_ms(500);
+    }
+}
+
